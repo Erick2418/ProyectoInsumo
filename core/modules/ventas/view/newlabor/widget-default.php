e7ef83691cab7e7e57f337e619ce7d06d1f90d3f
elfernando2418 <limefeja8995543@estudiantes3.edu.ec>
2022-02-22 21:56:06 -0500

    validaciones

--- core/modules/ventas/view/newlabor/widget-default.php
+++ core/modules/ventas/view/newlabor/widget-default.php
@@ -1,8 +1,9 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <div class="row">
 	<div class="col-md-12">
 	<h1>Nueva labor</h1>
 	<br>
-		<form class="form-horizontal" method="post" id="addlabor" action="index.php?view=addLabores" role="form">
+		<form class="form-horizontal" onsubmit="return validarDatos()"  method="post" id="addlabor" action="index.php?view=addLabores" role="form">
 
 
   <div class="form-group">
@@ -14,7 +15,7 @@
    <div class="form-group">
     <label for="descripcion" class="col-lg-2 control-label">Descripción*</label>
     <div class="col-md-6">
-      <input type="text" name="descripcion" class="form-control" required id="descripcion" placeholder="Descripción">
+      <input type="text" name="descripcion" class="form-control"  id="descripcion" placeholder="Descripción">
     </div>
   </div>
   
@@ -26,3 +27,27 @@
 </form>
 	</div>
 </div>
+<script>
+  function validarDatos() {
+
+    let nombre = document.getElementById("nombre").value;
+
+    let descripcion = document.getElementById("descripcion").value;
+
+    if (!nombre.trim()) {
+        swal("Debe ingresar un Nombre", '', 'warning');
+        console.log('debe ingresar categoria');
+        return false;
+    }
+    if (!descripcion.trim()) {
+        swal("Debe ingresar una Descripcion", '', 'warning');
+        console.log('debe ingresar catnidad');
+        return false
+    }
+
+
+    console.log('here')
+    return true;
+  }
+
+</script>
--- core/modules/ventas/view/newlote/widget-default.php
+++ core/modules/ventas/view/newlote/widget-default.php
@@ -1,28 +1,30 @@
-    <?php 
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  
+<?php 
 $categories = CategoryData::getAll();
     ?>
 <div class="row">
 	<div class="col-md-12">
 	<h1>Nuevo Lote</h1>
 	<br>
-		<form class="form-horizontal" method="post" enctype="multipart/form-data" id="addproduct" action="index.php?view=addLot" role="form">
+		<form class="form-horizontal" onsubmit="return validarDatos()"  method="post" enctype="multipart/form-data" id="addproduct" action="index.php?view=addLot" role="form">
 
   <div class="form-group">
     <label for="name" class="col-lg-2 control-label">Nombre *</label>
     <div class="col-md-6">
-      <input type="text" name="name" class="form-control" required id="name" placeholder="Descripción">
+      <input type="text" name="name" class="form-control"  id="name" placeholder="Descripción">
     </div>
   </div>
   <div class="form-group">
     <label for="num_lot" class="col-lg-2 control-label">Número</label>
  <div class="col-md-6">
-      <input type="number" name="num_lot" class="form-control" required id="num_lot" placeholder="Descripción">
+      <input type="number" name="num_lot" class="form-control"  id="num_lot" placeholder="Descripción">
     </div>
 </div>
 <div class="form-group" style="display:none;">
     <label for="dimension" class="col-lg-2 control-label">Dimensión del lote*</label>
     <div class="col-md-6">
-      <input type="text" name="dimension" value="5000" class="form-control" required id="dimension" placeholder="Descripción">
+      <input type="text" name="dimension" value="5000" class="form-control"  id="dimension" placeholder="Descripción">
    </div>
      </div>
   
@@ -37,3 +39,27 @@ $categories = CategoryData::getAll();
 
 	</div>
 </div>
+<script>
+  function validarDatos() {
+
+    let name = document.getElementById("name").value;
+
+    let num_lote = document.getElementById("num_lot").value;
+
+    if (!name.trim()) {
+        swal("Debe ingresar un Nombre", '', 'warning');
+        console.log('debe ingresar categoria');
+        return false;
+    }
+    if (!num_lote.trim()) {
+        swal("Debe ingresar un numero de lote", '', 'warning');
+        console.log('debe ingresar catnidad');
+        return false
+    }
+
+
+    console.log('here')
+    return true;
+  }
+
+</script>
